8ad2825531b956ced06be987d10efbca0d2e5b11
joamoyano <125034082+joamoyano@users.noreply.github.com>
2024-02-01 09:47:35 -0300

    git push -u origin master

---
+++ js/questions.js
@@ -0,0 +1,114 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "¿Cuál de los siguientes juegos pertenece al género de battle royale?",
+    answer: "Apex Legends",
+    options: [
+      "The Elder Scrolls V: Skyrim",
+      "Apex Legends",
+      "Stardew Valley",
+      "Dark Souls"
+    ]
+  },
+    {
+    numb: 2,
+    question: "En el juego Super Mario Bros., ¿cuál es el nombre del hermano de Mario?",
+    answer: "Luigi",
+    options: [
+      "Yoshi",
+      "Toad",
+      "Bowser",
+      "Luigi"
+    ]
+  },
+    {
+    numb: 3,
+    question: "En el juego League of Legends, ¿cuál de los siguientes roles no es uno de los roles principales?",
+    answer: "Ingeniero",
+    options: [
+      "Tanque",
+      "Asesino",
+      "Sanador",
+      "Ingeniero"
+    ]
+  },
+    {
+    numb: 4,
+    question: "En el juego The Witcher 3: Wild Hunt, ¿cuál es el nombre del protagonista?",
+    answer: "Geralt de Rivia",
+    options: [
+      "Ezio Auditore",
+      "Nathan Drake",
+      "Lara Croft",
+      "Geralt de Rivia"
+    ]
+  },
+    {
+    numb: 5,
+    question: "En la serie de juegos Fallout, ¿cuál es el nombre del refugio subterráneo donde los personajes suelen comenzar su aventura?",
+    answer: "Refugio 76",
+    options: [
+      "Refugio 76",
+      "Refugio 22",
+      "Refugio 13",
+      "Refugio 101"
+    ]
+  },
+
+    {
+    numb: 6,
+    question: "¿Cuál es el título del juego que presenta al personaje Kratos, un dios espartano que busca venganza?",
+    answer: "God of War",
+    options: [
+      "Assassin's Creed",
+      "Dark Souls",
+      "Devil May Cry",
+      "God of War"
+    ]
+   },
+   {
+    numb: 7,
+    question: "En el juego Red Dead Redemption 2, ¿cuál es el nombre del protagonista, un forajido en el Viejo Oeste?",
+    answer: "Arthur Morgan",
+    options: [
+      "John Marston",
+      "Dutch van der Linde",
+      "Javier Escuella",
+      "Arthur Morgan"
+    ]
+   },
+   {
+    numb: 8,
+    question: " En el juego World of Warcraft, ¿cuál es la raza conocida por su destreza en la magia y su capital en Dalaran?",
+    answer: "Elfos de la Noche",
+    options: [
+      "Enanos",
+      "Elfos de la Noche",
+      "Elfos de Sangre",
+      "Gnomos"
+    ]
+   },
+   {
+    numb: 9,
+    question: "¿Cuál es el título del juego que presenta a los personajes Ellie y Joel en un mundo postapocalíptico?",
+    answer: "The Last of Us",
+    options: [
+      "Left 4 Dead",
+      "The Last of Us",
+      "Days Gone",
+      "State of Decay"
+    ]
+   },
+   {
+    numb: 10,
+    question: "En el juego The Legend of Zelda: Breath of the Wild, ¿cuál es el nombre del reino en el que se desarrolla la aventura?",
+    answer: "Hyrule",
+    options: [
+      "Hyrule",
+      "Koholint",
+      "Eldin",
+      "Termina"
+    ]
+   },
+];
