a85ec5a8a5ea4613ffe20d6c580a5149f2f89a95
Moyano Joaquin <125034082+joamoyano@users.noreply.github.com>
2025-08-07 05:09:31 -0300

    refactor: guard DOM operations

---
+++ js/__tests__/script.test.js
@@ -0,0 +1,46 @@
+const { JSDOM } = require('jsdom');
+
+describe('optionSelected', () => {
+  let optionSelected, state;
+
+  beforeEach(() => {
+    jest.resetModules();
+    const dom = new JSDOM(`
+      <body>
+        <div class="option_list">
+          <div class="option"><span>A</span></div>
+          <div class="option"><span>B</span></div>
+          <div class="option"><span>C</span></div>
+          <div class="option"><span>D</span></div>
+        </div>
+        <footer><div class="next_btn"></div></footer>
+      </body>
+    `);
+    global.window = dom.window;
+    global.document = dom.window.document;
+    global.questions = [{
+      numb: 1,
+      question: 'Q',
+      answer: 'B',
+      options: ['A','B','C','D']
+    }];
+    ({ optionSelected, state } = require('../script'));
+    state.userScore = 0;
+    state.que_count = 0;
+  });
+
+  test('increments userScore on correct answer', () => {
+    const option = document.querySelectorAll('.option')[1];
+    optionSelected(option);
+    expect(state.userScore).toBe(1);
+  });
+
+  test('disables all options after selection', () => {
+    const option = document.querySelectorAll('.option')[1];
+    optionSelected(option);
+    const options = document.querySelectorAll('.option');
+    options.forEach(opt => {
+      expect(opt.classList.contains('disabled')).toBe(true);
+    });
+  });
+});
